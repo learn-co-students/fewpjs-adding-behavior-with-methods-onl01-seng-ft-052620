55ceb30f3661d7922eda283ac690adcad01bd9b2
Eyeruz <3yerus@gmail.com>
2020-09-10 12:47:45 -0400

    Done.

--- index.js
+++ index.js
@@ -2,29 +2,35 @@
 
 class Cat {
 constructor(name, sex){
+    this.name = name
+    this.sex = sex }
+    
     speak() {
-        `${this.name} says meow`
+        return `${this.name} says meow!`
     }
-}
 }
 
+
 class Dog {
-constructor(name, sex){
+constructor(name, sex) {
+    this.name = name
+    this.sex = sex }
+
     speak() {
-        `${this.name} says woof`
+         return `${this.name} says woof!`
     }
 }
-}
+
 
 class Bird {
 constructor(name, sex){
-    speak() { 
-if(this.sex == male){
-        `${this.name}, the parrot `
-    }
-    else 
-    `${this.name} says squawk `
+    this.name = name
+    this.sex = sex }
 
-}
-}
+    speak() { if(this.sex == "male") {
+        return `It's me! ${this.name}, the parrot!`
+   }
+   else
+   return `${this.name} says squawk!`
+    }
 }
