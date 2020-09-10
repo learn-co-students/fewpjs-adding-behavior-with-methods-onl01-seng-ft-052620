13ea358fc33ba2d0a3b0cec4eef1820bfa0100b0
Eyeruz <3yerus@gmail.com>
2020-09-10 12:39:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+
+class Cat {
+constructor(name, sex){
+    speak() {
+        `${this.name} says meow`
+    }
+}
+}
+
+class Dog {
+constructor(name, sex){
+    speak() {
+        `${this.name} says woof`
+    }
+}
+}
+
+class Bird {
+constructor(name, sex){
+    speak() { 
+if(this.sex == male){
+        `${this.name}, the parrot `
+    }
+    else 
+    `${this.name} says squawk `
+
+}
+}
+}
